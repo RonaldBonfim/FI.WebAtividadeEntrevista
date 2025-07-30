ef50f8a6954c47cee935fcb022bbd70236db4a52
RonaldBonfim <ronal.sousa.bonfim@gmail.com>
2025-07-29 21:35:04 -0300

    Criação de arquivos js para beneficiario. Criação de utilitarios para uso de forma geral. Adição de pacote de mascara para CPF. Adição de atributo de validação de CPF

--- FI.WebAtividadeEntrevista/Scripts/Clientes/FI.Clientes.js
+++ FI.WebAtividadeEntrevista/Scripts/Clientes/FI.Clientes.js
@@ -1,58 +1,56 @@
 ﻿
 $(document).ready(function () {
-    $('#formCadastro').submit(function (e) {
-        e.preventDefault();
-        $.ajax({
-            url: urlPost,
-            method: "POST",
-            data: {
-                "NOME": $(this).find("#Nome").val(),
-                "CEP": $(this).find("#CEP").val(),
-                "Email": $(this).find("#Email").val(),
-                "Sobrenome": $(this).find("#Sobrenome").val(),
-                "Nacionalidade": $(this).find("#Nacionalidade").val(),
-                "Estado": $(this).find("#Estado").val(),
-                "Cidade": $(this).find("#Cidade").val(),
-                "Logradouro": $(this).find("#Logradouro").val(),
-                "Telefone": $(this).find("#Telefone").val()
-            },
-            error:
-            function (r) {
-                if (r.status == 400)
-                    ModalDialog("Ocorreu um erro", r.responseJSON);
-                else if (r.status == 500)
-                    ModalDialog("Ocorreu um erro", "Ocorreu um erro interno no servidor.");
-            },
-            success:
-            function (r) {
-                ModalDialog("Sucesso!", r)
-                $("#formCadastro")[0].reset();
-            }
-        });
-    })
-    
+
+    $('#CPF').mask('000.000.000-00', { reverse: true });
+    $('#CPFBeneficiario').mask('000.000.000-00', { reverse: true });
+    $('#Telefone').mask('(00) 00000-0000');
+
+    $('#formCadastro').submit(function (event) {
+        event.preventDefault();
+        incluirClientes();
+    });    
 })
 
-function ModalDialog(titulo, texto) {
-    var random = Math.random().toString().replace('.', '');
-    var texto = '<div id="' + random + '" class="modal fade">                                                               ' +
-        '        <div class="modal-dialog">                                                                                 ' +
-        '            <div class="modal-content">                                                                            ' +
-        '                <div class="modal-header">                                                                         ' +
-        '                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>         ' +
-        '                    <h4 class="modal-title">' + titulo + '</h4>                                                    ' +
-        '                </div>                                                                                             ' +
-        '                <div class="modal-body">                                                                           ' +
-        '                    <p>' + texto + '</p>                                                                           ' +
-        '                </div>                                                                                             ' +
-        '                <div class="modal-footer">                                                                         ' +
-        '                    <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>             ' +
-        '                                                                                                                   ' +
-        '                </div>                                                                                             ' +
-        '            </div><!-- /.modal-content -->                                                                         ' +
-        '  </div><!-- /.modal-dialog -->                                                                                    ' +
-        '</div> <!-- /.modal -->                                                                                        ';
+function incluirClientes() {
+    var cliente = {
+        Nome: $('#formCadastro #Nome').val(),
+        Sobrenome: $('#formCadastro #Sobrenome').val(),
+        CPF: $('#formCadastro #CPF').val(),
+        Nacionalidade: $('#formCadastro #Nacionalidade').val(),
+        CEP: $('#formCadastro #CEP').val(),
+        Estado: $('#formCadastro #Estado').val(),
+        Cidade: $('#formCadastro #Cidade').val(),
+        Logradouro: $('#formCadastro #Logradouro').val(),
+        Email: $('#formCadastro #Email').val(),
+        Telefone: $('#formCadastro #Telefone').val(),
+        Beneficiarios: []
+    };
 
-    $('body').append(texto);
-    $('#' + random).modal('show');
+    $('#listaBeneficiarios tbody tr').each(function () {
+        var beneficiario = {
+            Id: $(this).find('td:eq(0)').text().trim(),
+            CPF: $(this).find('td:eq(1)').text().trim(),
+            Nome: $(this).find('td:eq(2)').text().trim()
+        };
+
+        cliente.Beneficiarios.push(beneficiario);
+    });
+
+    $.ajax({
+        url: urlPost,
+        type: 'POST',
+        data: JSON.stringify(cliente),
+        contentType: 'application/json',
+        success: function (result) {
+            const modalId = ModalDialog("Sucesso", "O Cliente foi incluído com sucesso!");
+
+            $('#' + modalId).on('hidden.bs.modal', function () {
+                window.location.href = urlRetorno;
+            });
+        },
+        error: function (error) {
+            const response = JSON.parse(error.responseText);
+            ModalDialog("Erro", "Não foi possível incluir o cliente. Detalhes: " + response.Message);
+        }
+    });
 }
